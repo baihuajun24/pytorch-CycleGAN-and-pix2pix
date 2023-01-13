67fa56d70ed15880d2e1571f9925e378a8dac812
Huajun Bai <baihuajun24@gmail.com>
2023-01-13 22:08:27 +0800

    add script to copy ckpt

---
+++ make_ckpt.sh
@@ -0,0 +1,5 @@
+# copy pretrain model ckpt for new fine-tune experiments
+cp checkpoints/freeze-finetune/200_net_D_A.pth checkpoints/$1
+cp checkpoints/freeze-finetune/200_net_D_B.pth checkpoints/$1
+cp checkpoints/freeze-finetune/200_net_G_A.pth checkpoints/$1
+cp checkpoints/freeze-finetune/200_net_G_B.pth checkpoints/$1
