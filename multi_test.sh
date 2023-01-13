e7877adb2a5a24bd774b95505eef70e157b5c155
Huajun Bai <baihuajun24@gmail.com>
2023-01-13 22:28:04 +0800

    multi_test modified

--- multi_test.sh
+++ multi_test.sh
@@ -3,5 +3,5 @@
 for i in {10..200..10}; 
 do
 	echo $i
-python test.py --dataroot /root/autodl-tmp/pytorch-CycleGAN-and-pix2pix/datasets/zdq_256_bw --name freeze-finetune --model cycle_gan --epoch $i > log/test_$i.log
+python test.py --dataroot datasets/zdq_256_bw --name freeze-4blocks --model cycle_gan --epoch $i > log/test_$i.log
 done
